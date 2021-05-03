f66433a0c88af70565c36218e7d788045db5bb84
DanielBL01 <dblee0532@gmail.com>
2021-05-03 16:45:37 -0400

    remove timer

--- src/components/Habitat.js
+++ src/components/Habitat.js
@@ -1,57 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const delay = Math.floor(Math.random() * (10 - 5) + 5);
-
 function Habitat(props) {
   const [pokemon, setPokemon] = useState([]);
-  const [battle, setBattle] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios.get(`/habitat?index=${props.habitat}`);
       setPokemon(result.data);
     }
     fetchData();
-
-    let timer = setTimeout(() => setBattle(true), delay * 1000);
-    return () => {
-      clearTimeout(timer);
-    }
   }, [props]);
+
+  console.log(pokemon);
 
   function handleClick(e) {
     e.preventDefault();
     props.setPage('Town');
   }
-  
-  console.log(pokemon);
-  
+
+  let display;
+  if (pokemon.length > 0) {
+    display = <div>
+          {pokemon.map(detail => { 
+            return (
+              <ul>
+                <li>{detail.name}</li>
+                <li>{detail.height}</li>
+                <li>{detail.weight}</li>
+                {detail.stats.map(stats => {
+                  return (
+                    <ul>
+                      <li>{stats.hp}</li>
+                      <li>{stats.attack}</li>
+                      <li>{stats.defense}</li>
+                      <li>{stats.speed}</li>
+                  </ul>
+                  )
+                })}
+                {detail.types.map(type => {
+                  return (
+                    <ul>
+                      <li>{type}</li>
+                    </ul>
+                  )
+                })}
+              </ul> 
+            )
+          })}
+      </div>
+  }
+
+  else {
+    display = <div>
+        Looking for Wild Pokemon...
+        <button onClick = {handleClick}>Run!</button>
+      </div>
+  }
+
   return (
     <div>
-      {battle === false && <div>
-        <div>Searching for Wild Pokémon...</div>
-        <button onClick = {handleClick}>
-          Go back to Town  
-        </button>
-      </div>}
-      {battle === true && <div>
-        <ul>
-        {pokemon.map(pokemon => {
-          return <li>{pokemon.name}</li>
-        })}
-        </ul>
-        <button onClick = {handleClick}>
-          Run! 
-        </button>
-      </div>}
+      {display}
     </div>
   );
 }
 
 export default Habitat;
-
-/*
-
-https://stackoverflow.com/questions/53090432/react-hooks-right-way-to-clear-timeouts-and-intervals
-
-*/
